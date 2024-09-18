58da344b887281f3b54a7037224c9fe4add9b622
hammadfareedH14 <166123272+hammadfareedH14@users.noreply.github.com>
2024-09-18 01:03:23 -0700

    station.py

--- MES/models/station.py
+++ MES/models/station.py
@@ -6,8 +6,12 @@ class StationModel(CommonModel, SurrogatePK):
     __tablename__ = "station"
     name = db.Column(db.String(100), nullable=False)
     line_id = db.Column(db.Integer, db.ForeignKey('line.id'), nullable=False)
+
+     # Relationships
     lines = relationship("LineModel", back_populates="stations")
     locations = relationship("LocationModel", back_populates="stations")
+    current_nodes = relationship("NodeModel", foreign_keys='NodeModel.current_station_id', back_populates="current_station")
+    next_nodes = relationship("NodeModel", foreign_keys='NodeModel.next_station_id', back_populates="next_station")
 
 
 
