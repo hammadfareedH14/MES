cc025416b7c5b260735d68701ad5e4ebe85fee0c
hammadfareedH14 <166123272+hammadfareedH14@users.noreply.github.com>
2024-09-20 05:46:07 -0700

    line.py

--- MES/models/line.py
+++ MES/models/line.py
@@ -1,9 +1,12 @@
 from db import db
 from models.abstract import CommonModel,SurrogatePK
 from sqlalchemy.orm import relationship
+
 class LineModel(CommonModel, SurrogatePK):
     __tablename__ = "line"
     name = db.Column(db.String(100), nullable=False)
-    shop_id = db.Column(db.Integer, db.ForeignKey('shop.id'), nullable=False)
-    shopes = relationship("ShopModel", back_populates="lines")
-    stations = relationship("StationModel", back_populates="lines")
+    # foreign key 
+    shop_id = db.Column(db.Integer, db.ForeignKey('shop.id'), nullable=False) 
+    # relationship 
+    shops = relationship("ShopModel", back_populates="lines") # shop
+    stations = relationship("StationModel", back_populates="lines") # station 
