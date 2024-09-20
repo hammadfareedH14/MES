a71b4b3de94155f4bac2c005f946a8e60cb220e1
hammadfareedH14 <166123272+hammadfareedH14@users.noreply.github.com>
2024-09-19 23:45:55 -0700

    MES directory

--- MES/models/line.py
+++
@@ -1,9 +0,0 @@
-from db import db
-from models.abstract import CommonModel,SurrogatePK
-from sqlalchemy.orm import relationship
-class LineModel(CommonModel, SurrogatePK):
-    __tablename__ = "line"
-    name = db.Column(db.String(100), nullable=False)
-    shop_id = db.Column(db.Integer, db.ForeignKey('shop.id'), nullable=False)
-    shopes = relationship("ShopModel", back_populates="lines")
-    stations = relationship("StationModel", back_populates="lines")
--- MES/models/role.py
+++
@@ -1,7 +0,0 @@
-from models.abstract import CommonModel,SurrogatePK
-from db import db
-
-class RoleModel(CommonModel, SurrogatePK):
-    __tablename__ = "role"
-    name = db.Column(db.String(255), unique=True, nullable=False)
-    description = db.Column(db.String(255), nullable=False)
