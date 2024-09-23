7a86bb74074f8310476fde435f906e9699e549e2
hammadfareedH14 <166123272+hammadfareedH14@users.noreply.github.com>
2024-09-23 01:02:47 -0700

    part.py

--- MES/models/part.py
+++ MES/models/part.py
@@ -1,10 +1,15 @@
-from db import db 
-from models.abstract import CommonModel,SurrogatePK
-# from sqlalchemy.orm import relationship
-
+from db import db  
+from models.abstract import CommonModel, SurrogatePK
+from sqlalchemy.orm import relationship
 class PartModel(CommonModel, SurrogatePK):
-    __tablename__="part"
+    __tablename__ = "part"
     description = db.Column(db.String(200), nullable=False)
     number = db.Column(db.Integer, nullable=False)
-    route_id = db.Column(db.Integer, db.ForeignKey('route.id'), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    
+    # Foreign key
+    route_id = db.Column(db.Integer, db.ForeignKey('route.id'), nullable=False)  
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False) 
+    
+    # Relationships
+    categories = relationship('CategoryModel', back_populates='parts')  
+    routes = relationship('RouteModel', back_populates='parts')  
