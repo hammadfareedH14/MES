94a28908039782d33634da5afa8f258e8cbe0ab1
hammadfareedH14 <166123272+hammadfareedH14@users.noreply.github.com>
2024-09-23 01:11:43 -0700

    location.py

--- MES/models/location.py
+++ MES/models/location.py
@@ -5,9 +5,11 @@ from sqlalchemy.orm import relationship
 class LocationModel(CommonModel, SurrogatePK):
     __tablename__ = "location"
     name = db.Column(db.String(100), nullable=False)
+    
+    # ForeignKey
     station_id = db.Column(db.Integer, db.ForeignKey('station.id'), nullable=False)
 
-#   Define relationship
-    stations = relationship("StationModel", back_populates="locations")
-    users = relationship('UserModel', back_populates='location')
+#   Relationship
+    stations = relationship("StationModel", back_populates="locations") 
+    users = relationship('UserModel', back_populates='locations') 
     
