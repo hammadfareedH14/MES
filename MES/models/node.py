a117c495d8db302d77424ccaea44c2053066e8e3
hammadfareedH14 <166123272+hammadfareedH14@users.noreply.github.com>
2024-09-23 06:31:46 -0700

    node.py

--- MES/models/node.py
+++ MES/models/node.py
@@ -6,13 +6,15 @@ from sqlalchemy.orm import relationship
 class NodeModel(CommonModel, SurrogatePK):
     __tablename__ = 'node'
 
+    is_start = db.Column(db.Boolean, default=False)
+    is_end = db.Column(db.Boolean, default=False)
+
+    # Foreign key
     current_station_id = db.Column(db.String, db.ForeignKey('station.id'), nullable=False)
     next_station_id = db.Column(db.String, db.ForeignKey('station.id'), nullable=False)
     route_id = db.Column(db.String, db.ForeignKey('route.id'), nullable=False)
-    is_start = db.Column(db.Boolean, default=False)
-    is_end = db.Column(db.Boolean, default=False)
-    
+
     # Relationships
-    current_station = relationship("StationModel", foreign_keys=[current_station_id], back_populates="current_nodes")
-    next_station = relationship("StationModel", foreign_keys=[next_station_id], back_populates="next_nodes")
-    route = relationship("RouteModel", back_populates="nodes")
+    current_station = relationship("StationModel", foreign_keys=[current_station_id], back_populates="current_node")
+    next_station = relationship("StationModel", foreign_keys=[next_station_id], back_populates="next_node")
+    routes = relationship("RouteModel", back_populates="nodes")
